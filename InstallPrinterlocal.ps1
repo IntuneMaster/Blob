518ffc5e2553580d8c3d3ce7161199d1edcf756e
IntuneMaster <amolhambir1111@gmail.com>
2025-08-22 13:34:38 +0530

    Add files via upload

---
+++ InstallPrinterlocal.ps1
@@ -0,0 +1,8 @@
+﻿$printerPath = "\\ensb02-prnt1\Blue_Ash_AR_2_UB"
+$existingPrinter = Get-Printer -Name $printerPath -ErrorAction SilentlyContinue
+if (-not $existingPrinter) {   
+    Add-Printer -ConnectionName $printerPath
+    Write-Output "Printer Installed"
+} else {
+    Write-Output "Printer exists."
+}
